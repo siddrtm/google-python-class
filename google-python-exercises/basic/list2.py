30fd3ce3db78bdf3e044bfa93daa25943bcfbf7c
Siddharth Sharma <siddrtm@yahoo.com>
2016-06-10 04:55:07 +0530

    completed list2.py exercise

--- google-python-exercises/basic/list2.py
+++ google-python-exercises/basic/list2.py
@@ -13,8 +13,11 @@
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
 def remove_adjacent(nums):
-  # +++your code here+++
-  return
+  res=[]
+  for num in nums:
+    if len(res)==0 or res[-1]!=num:
+      res.append(num)
+  return res
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -22,8 +25,22 @@ def remove_adjacent(nums):
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
 def linear_merge(list1, list2):
-  # +++your code here+++
-  return
+  i,j = 0,0
+  res =[]
+  while i<len(list1) and j<len(list2):
+    if list1[i] < list2[j]:
+      res.append(list1[i])
+      i+=1
+    else:
+      res.append(list2[j])
+      j+=1
+  while i<len(list1):
+    res.append(list1[i])
+    i+=1
+  while j<len(list2):
+    res.append(list2[j])
+    j+=1
+  return res
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
